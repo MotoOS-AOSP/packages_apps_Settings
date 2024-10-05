a0dbd80f6ec2a03daa4ed15157df0c2486106b54
seba3567 <sebastian26481@gmail.com>
2024-10-05 18:06:23 -0300

    fix synlink 2

--- TweakHouse/src/com/tweak/house/TopLevelExtensionsEntryPreferenceController.java
+++ TweakHouse/src/com/tweak/house/TopLevelExtensionsEntryPreferenceController.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.Tweak.house;
+package com.tweak.house;
 
 import android.content.Context;
 
--- TweakHouse/src/com/tweak/house/TweakHouse.java
+++ TweakHouse/src/com/tweak/house/TweakHouse.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.Tweak.house;
+package com.tweak.house;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
